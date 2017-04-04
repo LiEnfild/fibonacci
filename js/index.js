616ec99fa3598685c10234b8dbbdaf7009c22c15
LiEnfild <stasgon@bk.ru>
2017-04-04 13:28:27 +0000

    index.html

--- js/index.js
+++ js/index.js
@@ -1,25 +1,26 @@
 // JavaScript File
 function cycle(n) {
-    var fibFor1 = 1,
-        fibFor2 = 1;
+    var fibonacciCycle1 = 1,
+        fibonacciCycle2 = 1;
     for (var i = 3; i <= n; i++) {
-        var fibFor3 = fibFor1 + fibFor2;
-        fibFor1 = fibFor2;
-        fibFor2 = fibFor3;
+        var fibonacciCycle3 = fibonacciCycle1 + fibonacciCycle2;
+        fibonacciCycle1 = fibonacciCycle2;
+        fibonacciCycle2 = fibonacciCycle3;
     }
-    return fibFor2;
+    return fibonacciCycle2;
 }
 
 function recursion(n) {
-    var fibonacciRec;
+    var fibonacciRecursion;
     if (n >= 2) {
-        fibonacciRec = recursion(n - 1) + recursion(n - 2);
+        fibonacciRecursion = recursion(n - 1) + recursion(n - 2);
     }
     else {
-        fibonacciRec = n;
+        fibonacciRecursion = n;
     }
-    return fibonacciRec;
+    return fibonacciRecursion;
 }
-var argument = Number(prompt("Укажите порядковый номер члена ряда Фибоначчи(до 200)"));
-document.write(argument + '-й ряда Фибоначчи равен ' + recursion(argument) + '(Через рекурсию) <br>');
-document.write(argument + '-й ряда Фибоначчи равен ' + cycle(argument) + '(Через цикл) <br>');
+var argumentCycle = Number(prompt("Укажите порядковый номер члена ряда Фибоначчи (для цикла)"));
+var argumentRecursion = Number(prompt("Укажите порядковый номер члена ряда Фибоначчи (для рекурсии).Осторожно, при больших значениях возможно зависание!"));
+document.write(argumentRecursion + '-й ряда Фибоначчи равен ' + recursion(argumentRecursion) + '(Через рекурсию) <br>');
+document.write(argumentCycle + '-й ряда Фибоначчи равен ' + cycle(argumentCycle) + '(Через цикл) <br>');
